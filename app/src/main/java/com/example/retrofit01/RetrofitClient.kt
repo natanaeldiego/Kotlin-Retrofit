d571d8e3b0b5c35630bd9cf6c444d8e048b47a4b
Natanael Diego <natanaeldiego@gmail.com>
2020-09-04 00:26:06 -0300

    first commit

---
+++ app/src/main/java/com/example/retrofit01/RetrofitClient.kt
@@ -0,0 +1,29 @@
+package com.example.retrofit01
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient private constructor(){
+
+    companion object{
+        private lateinit var retrofit: Retrofit
+        private val baseUrl = "https://jsonplaceholder.typicode.com/"
+
+        private fun getRetrofitInstance(): Retrofit{
+            var httpClient = OkHttpClient.Builder()
+            if (!::retrofit.isInitialized) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .client(httpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+
+        fun <T> createService(serviceClass: Class<T>): T {
+            return getRetrofitInstance().create(serviceClass)
+        }
+    }
+}
